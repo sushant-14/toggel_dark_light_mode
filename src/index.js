043733de8307d524a53511de773064133ab7ccb6
Sushant Kumar <sushantprajapati1412@gmail.com>
2022-11-28 12:57:44 +0530

    mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var button=document.getElementById("btn");
+var mode=document.querySelector(".mode");
 button.addEventListener('click',function(){
     var element=document.body;
     element.classList.toggle("dark-mode")
+    mode.innerText="dark mode"
+    // event.stopPropagation();
 })
